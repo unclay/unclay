0cdbcb86c3d6efc027b2d38989384ed19a2397c4
clary <wcl58981298@gmail.com>
2015-07-27 21:36:59 +0800

    modify server.js

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-var http = require('http');
-var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+"use strict";
+var express = require("express");
+var os = require("os");
+var path = require("path");
+var jTemplate = require("juicer-template");
+
+var config = require("./config/config.json");
+var routes = require("./routes");
+
+
+var port = os.cpus()[0].model.indexOf("i5-4690") >= 0 ? 8085 : os.type().indexOf("Window") >= 0 ? 80 : 18080;
+
+var app = express();
+
+app.set("port", port);
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "html");
+app.use(jTemplate({
+	cache: false,
+	domain: config.domain
+}));
+
+
+routes(app);
+
+app.listen(app.get("port"), function() {
+    console.log("listen to " + app.get("port"));
+});
+
