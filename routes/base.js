0251c0d7a9ed7a2d53e00304babbd278083552da
clary <wcl58981298@gmail.com>
2015-08-11 22:42:52 +0800

    add note

--- routes/base.js
+++ routes/base.js
@@ -2,15 +2,21 @@
 var path = require("path");
 var fs = require("fs");
 // 首页
-exports.rindex = function(req, res) {
+exports.index = function(req, res) {
 	res.locals.layout = "layout"
     res.jrender("index", {
     	title: "unclay"
     });
 }
 // 网站历程
-exports.rhistory = function(req, res) {
+exports.history = function(req, res) {
     res.jrender("history", {
+    	title: "unclay"
+    });
+}
+// 博客
+exports.note = function(req, res) {
+    res.jrender("note", {
     	title: "unclay"
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var R_base = require("./base");
 module.exports = function(app){
-	app.get("/", R_base.rindex);
-	app.get("/history", R_base.rhistory);
+	app.get("/", R_base.index);
+	app.get("/history", R_base.history);
+	app.get("/note", R_base.note);
 }
