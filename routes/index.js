c5c16a0fbc0cf36258fa5e80f8bf4249592d5925
vagrant <vagrant@vagrant-bae.(none)>
2015-09-09 00:42:24 +0800

    add note list and note item page

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,14 @@
 var R_base = require("./base");
 var Model = require("./model");
 var R_admin = require("./admin");
+var R_note = require("./note");
+var marked = require("marked");
 module.exports = function(app){
 	R_admin(app);
 
 	app.get("/", R_base.index);
 	app.get("/history", R_base.history);
-	app.get("/note", R_base.note);
+	app.get("/note", R_note.note);
 
-	// app.get( "/note/:uri" , function(req, res, next){
-	// 	Model.Msl.use(function(callback){
-	// 		Model.Note
-	// 			.find()
-	// 			.where({
-	// 				seo_url: req.params.uri
-	// 			}, ["_id", "seo_title", "seo_keywords", "seo_description", "artical_title"])
-	// 			.exec(callback);
-	// 	}, function(err, data){
-	// 		if(err){
-	// 			next(err);
-	// 		} else if( isOwnEmpty(data) ){
-	// 			next(new Error());
-	// 		} else {
-	// 			res.jrender("note", {
-			    	
-	// 		    });
-	// 		}
-			
-	// 	});
-	// });
+	app.get( "/note/:uri" , R_note.note_item);
 }
--- routes/note.js
+++ routes/note.js
@@ -1,5 +1,36 @@
 "use strict";
-module.exports = function(app){
+var R_base = require("./base");
+var Model = require("./model");
+exports.note = function(req, res, next) {
+	Model.Msl.use(function(callback) {
+	    var p = Model.Note
+	        .find()
+	        .exec();
+	    p.then(function(data){
+	    	res.jrender("note", {
+		    	list: data
+		    });
+	    }).then(null, callback);
+	}, function(err) {
+	    next(err);
+	});
+}
 
 
+exports.note_item = function(req, res, next) {
+	Model.Msl.use(function(callback) {
+	    Model.Note
+	        .findOne()
+	        .where({
+	            seo_url: req.params.uri
+	        }, ["_id", "seo_title", "seo_keywords", "seo_description", "artical_title"])
+	        .exec(callback);
+	}, function(err, data) {
+	    if (err) {
+	        next(err);
+	    } else {
+	        data.content = marked(data.content);
+	        res.jrender("note_item", data);
+	    }
+	});
 }
